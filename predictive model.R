a36a19218c94b9a3469a477d45c95747652c05a3
Yijia Yu <scarlett@Yijias-MacBook-Pro.local>
2020-07-26 17:41:14 -0600

    update

--- predictive model.R
+++ predictive model.R
@@ -28,3 +28,32 @@ predicted.classes <- ifelse(probabilities > 0.5, "Yes", "No")
 mean(predicted.classes == df1[,  "post_inv_ped_only"])
 df[row.missing1.n, col.exsiting[-4]]
 
+#random forest imputation
+#install.packages("randomForest")
+library(randomForest)
+model1 <- randomForest(post_inv_ped_only ~ ., data = df[-99, c(col.exsiting[-4], "post_inv_ped_only")], importance = TRUE)
+df.estimate = df
+df.estimate[99,"post_inv_ped_only"]<- predict(model1, df[99 , col.exsiting[-4]])
+col.exsiting1 = c(col.exsiting, "post_inv_ped_only")
+model2 <- randomForest(post_inv_any ~ ., data = df.estimate[-99, c(col.exsiting1[-4], "post_inv_any")], importance = TRUE)
+df.estimate[99,"post_inv_any"]<- predict(model2, df.estimate[99 , col.exsiting1[-4]])
+col.exsiting1 = c(col.exsiting1, "post_inv_any")
+df.estimate[!complete.cases(df.estimate[,col.missing1]),]
+model3 <- randomForest(t2_disk_intensity ~ ., data = df.estimate[-46, c(col.exsiting1, "t2_disk_intensity")], importance = TRUE)
+df.estimate[46, "t2_disk_intensity"] <- predict(model3, df.estimate[46 , col.exsiting1])
+col.exsiting1 = c(col.exsiting1, "t2_disk_intensity")
+model4 <- randomForest(disk_inv ~ ., data = df.estimate[-46, c(col.exsiting1, "disk_inv")], importance = TRUE)
+df.estimate[46, "disk_inv"] <- predict(model4, df.estimate[46 , col.exsiting1])
+col.exsiting1 = c(col.exsiting1, "disk_inv")
+
+model5 <- randomForest(cord_compress ~ ., data = df.estimate[-39, c(col.exsiting1, "cord_compress")], importance = TRUE)
+df.estimate[39, "cord_compress"] <- predict(model5, df.estimate[39 , col.exsiting1])
+col.exsiting1 = c(col.exsiting1, "cord_compress")
+#col.missing1 is all imputed!
+colSums(is.na(df[,!colnames(df)%in% col.exsiting1])) 
+str(df.estimate)
+#continues variables
+cont_v = colnames(df.estimate)[!sapply(df, is.factor)]
+#maybe use pca to continues one later?
+model0 <- randomForest(dx_correct ~ ., data = df.estimate[, col.exsiting1[-c(2,3)]], importance = TRUE)
+varImpPlot(model0)
